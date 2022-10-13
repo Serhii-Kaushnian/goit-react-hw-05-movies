78d14e947ce6f30d3692e2050c8acbf3139d7f8b
Serhii Kaushnian <skaushnjan@gmail.com>
2022-10-13 11:48:29 +0200

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function Form({ getResponse }) {
   const [inputValue, setInputValue] = useState('');
-  const [query, setQuery] = useState('');
+  const [, setQuery] = useState('');
   const getMoviesBySearch = e => {
     e.preventDefault();
     setQuery(inputValue);
@@ -10,17 +10,6 @@ export default function Form({ getResponse }) {
   };
   const inputHandler = e => {
     setInputValue(e.currentTarget.value);
-  };
-  const [searchQuery, setSearchQuery] = useState('');
-  const [page, setPage] = useState(1);
-  const [response, setResponse] = useState([]);
-
-  const getMovies = query => {
-    console.log(query);
-    setSearchQuery(query);
-  };
-  const loadMore = () => {
-    setPage(state => state + 1);
   };
 
   return (
