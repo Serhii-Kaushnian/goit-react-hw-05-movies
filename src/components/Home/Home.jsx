5f887b698dcf409c9ce2be726fe6aa7a85af7acc
Serhii Kaushnian <skaushnjan@gmail.com>
2022-10-13 22:46:00 +0200

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -41,21 +41,22 @@ export default function Home({ handleResponse }) {
   return (
     <div>
       <GalleryWrapper>
-        {response.length > 0 &&
-          response.map(value => (
-            <Link to={`${value.id}`}>
-              <GalleryItemWrapper key={value.id}>
-                <GalleryItemImage
-                  src={`https://image.tmdb.org/t/p/original/${value.poster_path}`}
-                  alt={value.title ? value.title : value.name}
-                  width="350px"
-                />
-                <MovieTitle>
-                  {value.title ? value.title : value.name}
-                </MovieTitle>
-              </GalleryItemWrapper>
-            </Link>
-          ))}
+        {response.length > 0
+          ? response.map(value => (
+              <Link to={`${value.id}`}>
+                <GalleryItemWrapper key={value.id}>
+                  <GalleryItemImage
+                    src={`https://image.tmdb.org/t/p/original/${value.poster_path}`}
+                    alt={value.title ? value.title : value.name}
+                    width="350px"
+                  />
+                  <MovieTitle>
+                    {value.title ? value.title : value.name}
+                  </MovieTitle>
+                </GalleryItemWrapper>
+              </Link>
+            ))
+          : null}
       </GalleryWrapper>
       {loader && <Loader />}
 
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -15,21 +15,22 @@ export default function Home({ handleQuery, response, onClick, loader }) {
       <Form onSubmit={handleQuery} />
 
       <GalleryWrapper>
-        {response.length > 0 &&
-          response.map(value => (
-            <Link to={`${value.id}`}>
-              <GalleryItemWrapper key={value.id}>
-                <GalleryItemImage
-                  src={`https://image.tmdb.org/t/p/original/${value.poster_path}`}
-                  alt={value.title ? value.title : value.name}
-                  width="350px"
-                />
-                <MovieTitle>
-                  {value.title ? value.title : value.name}
-                </MovieTitle>
-              </GalleryItemWrapper>
-            </Link>
-          ))}
+        {response.length > 0
+          ? response.map(value => (
+              <Link to={`${value.id}`}>
+                <GalleryItemWrapper key={value.id}>
+                  <GalleryItemImage
+                    src={`https://image.tmdb.org/t/p/original/${value.poster_path}`}
+                    alt={value.title ? value.title : value.name}
+                    width="350px"
+                  />
+                  <MovieTitle>
+                    {value.title ? value.title : value.name}
+                  </MovieTitle>
+                </GalleryItemWrapper>
+              </Link>
+            ))
+          : null}
       </GalleryWrapper>
       {loader && <Loader />}
 
